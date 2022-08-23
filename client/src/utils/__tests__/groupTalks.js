64934118e49567f1a54ca4bea32c04b2c73d6022
Nathan Galante <galante.nathan@gmail.com>
2022-08-23 22:40:41 +0200

    input for tests done

---
+++ client/src/utils/__tests__/groupTalks.js
@@ -0,0 +1,27 @@
+import { groupTalks } from "../groupTalks";
+
+describe("group talks", () => {
+    it("group talks by time", () => {
+        const testInput = [
+            {title: 'Writing Fast Tests Against Enterprise Rails', time: 60},
+            {title: 'Overdoing it in Python', time: 45},
+            {title: 'Lua for the Masses', time: 30},
+            {title: 'Ruby Errors from Mismatched Gem Versions', time: 45},
+            {title: 'Common Ruby Errors', time: 45},
+            {title: 'Rails for Python Developers',time: 5},
+            {title: 'Communicating Over Distance', time: 60},
+            {title: 'Accounting-Driven Development', time: 45},
+            {title: 'Woah', time: 30},
+            {title: 'Sit Down and Write', time: 30},
+            {title: 'Pair Programming vs Noise', time: 45},
+            {title: 'Rails Magic', time: 60},
+            {title: 'Ruby on Rails: Why We Should Move On', time: 60},
+            {title: 'Clojure Ate Scala {title: on my project)', time: 45},
+            {title: 'Programming in the Boondocks of Seattle', time: 30},
+            {title: 'Ruby vs. Clojure for Back-End Development', time: 30},
+            {title: 'Ruby on Rails Legacy App Maintenance', time: 60},
+            {title: 'A World Without HackerNews', time: 30},
+            {title: 'User Interface CSS in Rails Apps', time: 30}
+        ];
+    });
+});
